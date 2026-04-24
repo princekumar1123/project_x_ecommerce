1b676d0b77a97561412dc1ffb1d5af5c003b161e
prince <prince.bc@mitrahsoft.com>
2026-04-24 18:48:36 +0530

    Fix the build issue

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,6 +1,6 @@
 const environment = {
+    // baseURL: "http://localhost:9000",
     baseURL: "https://prince-shoppify-server-23n4.onrender.com",
-    // baseURL:  "http://localhost:9000",
 };
 
 export default environment;
